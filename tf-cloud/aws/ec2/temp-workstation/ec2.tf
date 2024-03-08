63e5ca32da96e7db3c78d01ecb987bb0f61ead51
ss027503 <srs6c7@gmail.com>
2024-03-08 00:02:25 -0600

    file not saved

--- tf-cloud/aws/ec2/temp-workstation/ec2.tf
+++ tf-cloud/aws/ec2/temp-workstation/ec2.tf
@@ -33,29 +33,3 @@ resource "aws_instance" "this" {
         Name                = var.host_name
     }
 }
-
-resource "aws_security_group" "this" {
-    name            = "connect-from-home-sg"
-    vpc_id          = var.vpc_id
-
-    ingress {
-        from_port   = 3389
-        to_port     = 3389
-        protocol    = "tcp"
-        cidr_blocks = ["${var.home_ip}/32"]        
-    }
-
-    ingress {
-        from_port   = 22
-        to_port     = 22
-        protocol    = "tcp"
-        cidr_blocks = ["${var.home_ip}/32"]        
-    }
-
-    egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
