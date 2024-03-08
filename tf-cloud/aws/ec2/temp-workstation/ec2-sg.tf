522731e35e18423bc3ae513bb4b2f5491aef9333
ss027503 <srs6c7@gmail.com>
2024-03-08 00:10:12 -0600

    update sg

--- tf-cloud/aws/ec2/temp-workstation/ec2-sg.tf
+++ tf-cloud/aws/ec2/temp-workstation/ec2-sg.tf
@@ -1,28 +1,28 @@
-resource "aws_security_group" "this" {
-    name            = "connect-from-home-sg"
-    vpc_id          = var.vpc_id
+# resource "aws_security_group" "this" {
+#     name            = "connect-from-home-sg"
+#     vpc_id          = var.vpc_id
 
-    ingress {
-        from_port   = 3389
-        to_port     = 3389
-        protocol    = "tcp"
-        cidr_blocks = join(["${var.home_ip}/32", "${var.extra_ip}/32"])      
-    }
+#     ingress {
+#         from_port   = 3389
+#         to_port     = 3389
+#         protocol    = "tcp"
+#         cidr_blocks = join(["${var.home_ip}/32", "${var.extra_ip}/32"])      
+#     }
 
-    ingress {
-        from_port   = 22
-        to_port     = 22
-        protocol    = "tcp"
-        cidr_blocks = join(["${var.home_ip}/32", "${var.extra_ip}/32"])        
-    }
+#     ingress {
+#         from_port   = 22
+#         to_port     = 22
+#         protocol    = "tcp"
+#         cidr_blocks = join(["${var.home_ip}/32", "${var.extra_ip}/32"])        
+#     }
 
-    egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+#     egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
 
 module "aws_security_group" {
     source = "terraform-aws-modules/security-group/aws"
